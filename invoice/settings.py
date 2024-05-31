cf27591a05cbfdfa045e38db76340e5321ee1ed2
Sahil Bharadwaj <119106087+Sahil0215@users.noreply.github.com>
2024-05-31 13:33:19 +0530

    Update settings.py

--- invoice/settings.py
+++ invoice/settings.py
@@ -120,10 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-import os
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
