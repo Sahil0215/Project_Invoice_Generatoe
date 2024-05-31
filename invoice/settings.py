e67810683e6d584ff52d68cc09463343a5de4bd2
Sahil Bharadwaj <119106087+Sahil0215@users.noreply.github.com>
2024-05-31 13:37:09 +0530

    Update settings.py

--- invoice/settings.py
+++ invoice/settings.py
@@ -74,15 +74,10 @@ WSGI_APPLICATION = 'invoice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.dummy',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
